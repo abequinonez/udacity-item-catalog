faf06db2aeb95b4f4dc650725c451aef4151677a
abequinonez <abequinonez@users.noreply.github.com>
2017-11-06 23:51:00 -0800

    Add models

--- database_setup.py
+++ database_setup.py
@@ -5,6 +5,34 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class User(Base):
+    """Model class for storing user information."""
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+class Category(Base):
+    """Model class for storing a category."""
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+class Item(Base):
+    """Model class for storing an item."""
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    description = Column(String(300))
+    cat_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
 engine = create_engine('sqlite:///catalog.db')
 
 Base.metadata.create_all(engine)
