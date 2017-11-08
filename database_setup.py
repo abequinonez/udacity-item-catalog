50f4eb10f091048bb9ff86de99922a5fa72758d5
abequinonez <abequinonez@users.noreply.github.com>
2017-11-07 23:53:54 -0800

    Add code for populating database with categories

--- database_setup.py
+++ database_setup.py
@@ -27,7 +27,7 @@ class Item(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(80), nullable=False)
-    description = Column(String(300))
+    description = Column(String(800))
     cat_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
     user_id = Column(Integer, ForeignKey('user.id'))
@@ -36,3 +36,46 @@ class Item(Base):
 engine = create_engine('sqlite:///catalog.db')
 
 Base.metadata.create_all(engine)
+
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+def addCategories():
+    """Adds categories to the Category table."""
+
+    # Add the Chinese category
+    cat1 = Category(name='Chinese')
+    session.add(cat1)
+    session.commit()
+
+    # Add the Japanese category
+    cat2 = Category(name='Japanese')
+    session.add(cat2)
+    session.commit()
+
+    # Add the Korean category
+    cat3 = Category(name='Korean')
+    session.add(cat3)
+    session.commit()
+
+    # Add the Thai category
+    cat4 = Category(name='Thai')
+    session.add(cat4)
+    session.commit()
+
+    # Add the Vietnamese category
+    cat5 = Category(name='Vietnamese')
+    session.add(cat5)
+    session.commit()
+
+    # Add the Other category
+    cat6 = Category(name='Other')
+    session.add(cat6)
+    session.commit()
+
+    print('Categories added!')
+
+# Populate the database with categories (if they don't exist)
+if len(session.query(Category).all()) == 0:
+    addCategories()
