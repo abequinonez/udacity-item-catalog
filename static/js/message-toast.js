afc2fd03d78659970ae70a97e6477d72a2a6d800
abequinonez <abequinonez@users.noreply.github.com>
2017-12-15 23:41:15 -0800

    Move showToastMessage() out of if block

--- static/js/message-toast.js
+++ static/js/message-toast.js
@@ -1,15 +1,16 @@
+// Show the message sent by the server as an MDL toast
+function showToastMessage() {
+    let messageToast = document.querySelector('#message-toast');
+    let data = {message: flashMessage};
+    messageToast.MaterialSnackbar.showSnackbar(data);
+}
+
 /*
 If the flashMessage variable contains a value other than null, the
 showToastMessage() function will be called to show the message sent
 by the server (after the document has finished loading).
 */
 if (flashMessage) {
-    function showToastMessage() {
-        let messageToast = document.querySelector('#message-toast');
-        let data = {message: flashMessage};
-        messageToast.MaterialSnackbar.showSnackbar(data);
-    }
-
     // Call showToastMessage() when the document is ready
     const toastInterval = setInterval(function() {
         if (document.readyState === 'complete') {
